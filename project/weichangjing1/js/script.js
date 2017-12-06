24efddca500bff8aaf2171d361695abe0aedd0cc
有求必硬 <1176903671@qq.com>
2017-12-06 11:09:34 +0800

    .

---
+++ project/weichangjing1/js/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: false
+    },
+    navigation: {
+        nextEl: '.swiper-button-next'
+    }
+
+});
