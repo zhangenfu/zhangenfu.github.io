c203cb35a056bdc3bb1dfaf28554e3d04a411095
有求必硬 <1176903671@qq.com>
2017-12-11 09:35:35 +0800

    添加了音乐音乐

--- project/weichangjing1/js/script.js
+++ project/weichangjing1/js/script.js
@@ -19,3 +19,28 @@ var swiper = new Swiper('.swiper-container', {
     }
 
 });
+
+//音频标签
+var mp3 = document.querySelector("#mp3");
+
+//音乐按钮
+var mp3Btn = document.querySelector("#mp3Btn");
+
+//暂停音乐按钮
+//mp3Btn.style.animationPlayState = 'paused'
+//旋转音乐按钮
+//mp3Btn.style.animationPlayState = 'running'
+
+mp3Btn.onclick = function () {
+    if (mp3.paused) {
+        //开始旋转
+        mp3Btn.style.animationPlayState = 'running';
+        //开始音频
+        mp3.play();
+    } else {
+        //暂停旋转
+        mp3Btn.style.animationPlayState = 'paused';
+        //暂停音频
+        mp3.pause();
+    }
+};
