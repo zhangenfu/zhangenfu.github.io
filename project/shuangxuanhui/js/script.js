6cc8616c93180e9ef64f25bba77b9810c3585c58
有求必硬 <1176903671@qq.com>
2017-12-13 16:51:05 +0800

    新增双选会

--- project/shuangxuanhui/js/script.js
+++ project/shuangxuanhui/js/script.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper('.swiper-container', {
     direction: 'vertical',
-    effect : 'cube',
+    //effect : 'cube',
     pagination: {
         el: '.swiper-pagination',
         clickable: false
@@ -19,3 +19,30 @@ var swiper = new Swiper('.swiper-container', {
     }
 
 });
+
+
+//音频标签
+var mp3=document.querySelector("#mp3");
+//音乐按钮
+var mp3Btn=document.querySelector("#mp3Btn");
+
+//暂停音乐按钮
+//mp3Btn.style.animationPlayState="paused";
+//停止播放
+//mp3.pause();
+
+//音乐按钮旋转
+//mp3Btn.style.animationPlayState="running";
+//开始播放
+//mp3.play();
+
+mp3Btn.onclick=function(){
+    if(mp3.paused){
+        mp3Btn.style.animationPlayState="running";
+        mp3.play();
+    }else{
+        mp3Btn.style.animationPlayState="paused";
+
+        mp3.pause();
+    }
+};
