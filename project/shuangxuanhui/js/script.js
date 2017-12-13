535462e08eb0de6a851a74a394fd73f4794e3454
有求必硬 <1176903671@qq.com>
2017-12-13 14:35:59 +0800

    .

---
+++ project/shuangxuanhui/js/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    effect : 'cube',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: false
+    },
+    navigation: {
+        nextEl: '.swiper-button-next'
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+
+});
